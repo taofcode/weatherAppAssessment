83dfb0f6933df7d5ede6d371333306b592fc9012
Shelton Chiuswa <taofcode@users.noreply.github.com>
2022-09-21 07:49:07 +0200

    Initial Commit

---
+++ Assessment/AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentApp.swift
+//  Assessment
+//
+//  Created by shelton chiuswa on 21/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
